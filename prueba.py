f9c7524d54e03796968cfb389bfaba7dd8884b1f
josesolis-06 <124844596+josesolis-06@users.noreply.github.com>
2023-03-04 04:27:03 +0000

    tarea 04

--- prueba.py
+++ prueba.py
@@ -1,16 +1,16 @@
 import math
-r = math.sqrt((x-0)**2+(y-0)**2)
-print(r)
 
 def puntaje(x, y):
-if (5 < r <= 10):
-    print("1 punto")
-elif (1 < r <= 5):
-    print("5 puntos")
-elif (r <= 1):
-    print("10 puntos")
-elif(r>=10.1):
-    print("0 puntos")
+    r= puntaje(x,y)
+    if (5 < r <= 10):
+        print("1 punto")
+    elif (1 < r <= 5):
+        print("5 puntos")
+    elif (r <= 1):
+        print("10 puntos")
+    elif(r>=10.1):
+        print("0 puntos")
+r = math.sqrt((x)**2+(y)**2)
 
 puntaje(1,12)
 puntaje(3,6)
